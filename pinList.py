53f1012c5c8dd92676be899dc39b19f3a9fa4f27
Thorrak <thorrak@excelinspace.com>
2016-07-09 09:33:05 -0400

    Updating board definition for ESP8266

--- pinList.py
+++ pinList.py
@@ -132,10 +132,10 @@ def getPinList(boardType, shieldType):
                    {'val': 0, 'text': 'OneWire', 'type': 'onewire'}]
     elif (boardType == "esp8266"):  # Note - Excluding shield definition for now
         pinList = [{'val': 0, 'text': '  D3 (Cool)', 'type': 'act'},
-                   {'val': 13, 'text': '  D7 (Heat)', 'type': 'act'},
+                   {'val': 2, 'text': '  D4 (Heat)', 'type': 'act'},
                    {'val': 14, 'text': '  D5 (Door)', 'type': 'door'},
                    {'val': 12, 'text': 'D6 (OneWire)', 'type': 'onewire'},
-                   {'val': 2, 'text': 'D4 (Buzzer)', 'type': 'beep'},]
+                   {'val': 13, 'text': 'D7 (Buzzer)', 'type': 'beep'},]
     else:
         print 'Unknown controller or board type'
         pinList = {}
